6a53f6aa84ca36dd51b484cc1da49d9c9f1b857e
Michael Kocian <kocian.michael@gmail.com>
2021-01-03 23:35:04 +0100

    Project create

---
+++ War3Connect3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace War3Connect3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
